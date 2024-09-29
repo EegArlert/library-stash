c7808edf6436c68c7405daa2a2cdab326c738a83
EegArlert <bsyahputra@oakland.edu>
2024-09-29 19:23:06 -0400

    added html skeleton and some css styling

---
+++ library.js
@@ -0,0 +1,21 @@
+/* Library Stash*/
+
+
+/* Global Variable */
+
+/* book info storage */
+
+const myLibrary = [];
+
+
+/* AddingBook -- adding book to the array */
+
+
+
+
+
+/* DeleteBook -- deleting book from the array */
+
+
+
+
