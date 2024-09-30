91e887da5f200c81c3ed11afcb134ec4c1ad80ee
EegArlert <bsyahputra@oakland.edu>
2024-09-29 20:30:23 -0400

    write main functionalities of library.js (array, adding, removing)

--- library.js
+++ library.js
@@ -2,20 +2,29 @@
 
 
 /* Global Variable */
-
-/* book info storage */
-
 const myLibrary = [];
 
+/* book info storage object */
+
+function Book(title, author, numPages, isCompleted){
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.isCompleted = isCompleted;
+}
 
 /* AddingBook -- adding book to the array */
 
-
-
+function AddBookToLibrary(Book) {
+    myLibrary.push(Book)
+}
 
 
 /* DeleteBook -- deleting book from the array */
 
+function DeleteBook(position) {
+    myLibrary.splice(position, 1);
+}
 
 
 
